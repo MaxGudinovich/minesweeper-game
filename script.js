968869a253ae287da7c2d1e320904737c240eec2
Maksym Hudinovych <maksym.hudinovych@powercoders.org>
2024-05-03 12:16:34 +0200

    Added solution

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-
 // Set this constant to true to debug the placement of bombs without
 // having to click on all cells to reveal them.
 const CHEAT_REVEAL_ALL = false;
 
 const ROWS_COUNT = 10;
 const COLS_COUNT = 10;
+const BOMBS_COUNT = 10;
+let bombs = BOMBS_COUNT;
 
 var defeat = false;
 var victory = false;
 
-// Cell constructor
 function Cell() {
   this.discovered = false;
   this.isBomb = false;
   this.hasBeenFlagged = false;
 }
 
-// Initialize cells
 var cells = Array(ROWS_COUNT);
 for (var row = 0; row < ROWS_COUNT; row++) {
   cells[row] = Array(COLS_COUNT);
@@ -25,85 +24,105 @@ for (var row = 0; row < ROWS_COUNT; row++) {
   }
 }
 
-//
-// TODO: Task 1 - add some bombs at fixed positions.
-// cells[0][1].isBomb = true;
-// cells[5][4].isBomb = true;
-// cells[9][9].isBomb = true;
+let totalCellsToClear = cells.length * cells.length - BOMBS_COUNT;
 
-//
-// TODO: Task 2 - Comment out the code of task 1. Instead of adding bombs in fixed places, add 10 of them in random places.
-//                Add a BOMBS_COUNT constant so that you can easily change the amount of bombs placed. Put it next to the
-//                other constants.
-//
+while (bombs > 0) {
+  var row = Math.floor(Math.random() * ROWS_COUNT);
+  var col = Math.floor(Math.random() * COLS_COUNT);
+  if (!cells[row][col].isBomb) {
+    cells[row][col].isBomb = true;
+    bombs--;
+  }
+}
 
-
-// Once the game has been initialized, we "render" it.
 render();
 
+let clickedRow, clickedCol;
 
-//
-// Game functions definitions
-//
+function handleClick(row, col) {
+  clickedRow = row;
+  clickedCol = col;
 
-function discoverCell(row, col) {
-  //
-  // TODO: Task 5 - Reveal cells when clicked.
-  //
+  discoverCell(clickedRow, clickedCol);
+}
 
-  //
-  // TODO: Task 6 - Discover neighbor cells recursively, as long as there are no adjacent bombs to the current cell.
-  //
+function discoverCell(row, col, initialRow, initialCol) {
+  if (initialRow === undefined || initialCol === undefined) {
+    initialRow = row;
+    initialCol = col;
 
-  //
-  // TODO: Task 8 - Implement defeat. If the player "discovers" a bomb (clicks on it without holding shift), set the variable defeat to true.
-  //
+    if (cells[row][col].isBomb) {
+      defeat = true;
+    }
+  }
+
+  if (row < 0 || row >= cells.length || col < 0 || col >= cells[0].length) {
+    return;
+  }
+
+  if (cells[row][col].discovered) {
+    return;
+  }
+
+  if (cells[row][col].isBomb) {
+    return;
+  }
+
+  if (Math.abs(row - initialRow) > 1 || Math.abs(col - initialCol) > 1) {
+    return;
+  }
+
+  cells[row][col].discovered = true;
+
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      discoverCell(row + i, col + j, initialRow, initialCol);
+    }
+  }
 }
 
 function flagCell(row, col) {
-  //
-  // TODO: Task 7 - Implement flags. Flags allow the player to mark cells that they think contain a bomb.
-  //                When clicking a cell and holding shift, function flagCell() will be called for you.
-  //
+  cells[row][col].hasBeenFlagged = true;
 }
 
-// This function is called once for each cell when rendering the game. The row and col of the current cell is
-// passed to the functionn
 function countAdjacentBombs(row, col) {
-  //
-  // TODO: Task 4 - Adjacent bombs are bombs in cells touching our cell (also diagonally). Implement this function
-  //                so that it returns the count of adjacent cells with bombs in them. 
-  //
-  return 1;
+  let bombCount = 0;
+
+  for (let i = row - 1; i <= row + 1; i++) {
+    for (let j = col - 1; j <= col + 1; j++) {
+      if (i >= 0 && i < cells.length && j >= 0 && j < cells[0].length) {
+        if (cells[i][j].isBomb) {
+          bombCount++;
+        }
+      }
+    }
+  }
+  if (cells[row][col].isBomb) {
+    bombCount--;
+  }
+  return bombCount;
 }
 
 function getBombsCount() {
-  //
-  // TODO: Task 9 - Implement stats: the counters currently always display 0, calculate and return the relevant values.
-  //
-  return 0;
+  return BOMBS_COUNT;
 }
 
 function getClearedCells() {
-  //
-  // TODO: Task 9 - Implement stats: the counters currently always display 0, calculate and return the relevant values.
-  //
-  return 0;
+  let cleared = 0;
+  cells.map((el) => el.map((el) => (el.discovered ? cleared++ : '')));
+  return cleared;
 }
 
 function getTotalCellsToClear() {
-  //
-  // TODO: Task 9 - Implement stats: the counters currently always display 0, calculate and return the relevant values.
-  //
-  return 0;
+  return totalCellsToClear;
 }
 
 function checkForVictory() {
-  //
-  // TODO: Task 10 - Implement victory. If the player has revealed as many cells as they must (every cell that isn't a
-  //                 bomb), set variable victory to true.
-  //
-  return 0;
+  let cleared = getClearedCells();
+
+  if (cleared === totalCellsToClear) {
+    victory = true;
+  }
 }
 
 //
@@ -111,72 +130,74 @@ function checkForVictory() {
 //
 function getMessage() {
   if (victory == true) {
-    return "Well done! 👏🏼<br><br>Refresh the page to start again.";
+    return 'Well done! 👏🏼<br><br>Refresh the page to start again.';
   } else if (defeat) {
-    return "Boom! 💥<br><br>Refresh the page to try again.";
+    return 'Boom! 💥<br><br>Refresh the page to try again.';
   }
-  return "";
+  return '';
 }
 
 // "Render" the game. Update the content of the page to reflect any changes to the game state.
 function render() {
-  var playfield = document.getElementById("playfield");
-  var html = "";
+  var playfield = document.getElementById('playfield');
+  var html = '';
   for (var row = 0; row < ROWS_COUNT; row++) {
     html += '<div class="row">';
     for (var col = 0; col < COLS_COUNT; col++) {
       var cell = cells[row][col];
-      var cellText = "";
-      var cssClass = "";
-      var textColor = "";
+      var cellText = '';
+      var cssClass = '';
+      var textColor = '';
       if (cell.discovered || CHEAT_REVEAL_ALL || defeat) {
-        cssClass = "discovered";
+        cssClass = 'discovered';
         if (cell.isBomb) {
-          cellText = "💣";
+          cellText = '💣';
         } else {
           var adjBombs = countAdjacentBombs(row, col);
           if (adjBombs > 0) {
             cellText = adjBombs.toString();
             if (adjBombs == 1) {
-              textColor = "blue";
+              textColor = 'blue';
             } else if (adjBombs == 2) {
-              textColor = "green";
+              textColor = 'green';
             } else if (adjBombs == 3) {
-              textColor = "red";
+              textColor = 'red';
             } else if (adjBombs == 4) {
-              textColor = "black";
+              textColor = 'black';
             }
           }
         }
       } else {
         if (cell.hasBeenFlagged) {
-          cellText = "🚩"
+          cellText = '🚩';
         }
       }
       html += `<div class="cell ${cssClass}" style="color:${textColor}" onclick="onCellClicked(${row}, ${col}, event)">${cellText}</div>`;
     }
-    html += "</div>"
+    html += '</div>';
   }
   playfield.innerHTML = html;
 
   // Defeat screen
-  var body = document.getElementsByTagName("body")[0];
+  var body = document.getElementsByTagName('body')[0];
   if (defeat) {
-    body.classList.add("defeat")
+    body.classList.add('defeat');
   }
 
   // Victory screen
   if (victory) {
-    body.classList.add("victory")
+    body.classList.add('victory');
   }
 
   // Update stats
-  document.getElementById("bombs-count").innerText = getBombsCount().toString();
-  document.getElementById("cleared-cells-count").innerText = getClearedCells().toString();
-  document.getElementById("total-cells-to-clear").innerText = getTotalCellsToClear().toString();
+  document.getElementById('bombs-count').innerText = getBombsCount().toString();
+  document.getElementById('cleared-cells-count').innerText =
+    getClearedCells().toString();
+  document.getElementById('total-cells-to-clear').innerText =
+    getTotalCellsToClear().toString();
 
   // Update message
-  document.getElementById("message").innerHTML = getMessage();
+  document.getElementById('message').innerHTML = getMessage();
 }
 
 // This function gets called each time a cell is clicked. Arguments "row" and "col" will be set to the relevant
@@ -185,7 +206,8 @@ function onCellClicked(row, col, event) {
   if (event.shiftKey) {
     flagCell(row, col);
   } else {
-    discoverCell(row, col);
+    /* discoverCell(row, col); */
+    handleClick(row, col);
   }
   checkForVictory();
   render();
